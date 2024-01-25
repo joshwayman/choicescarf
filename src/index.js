c2fc01a7d32e7df53cd10b56e66718aebc1cd420
joshwayman <nowayman121@gmail.com>
2024-01-25 14:50:34 +1100

    Added GTM

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-W7XHSG",
+};
+
+TagManager.initialize(tagManagerArgs);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
